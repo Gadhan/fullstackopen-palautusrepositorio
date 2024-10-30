2ebd49e6572cfe4fd2a55cdda29e92df2d5d1540
mikaelti <mikaelti@metropolia.fi>
2024-10-30 14:39:50 +0200

    4.10

--- osa4/blogilista/tests/bloglist_api.test.js
+++ osa4/blogilista/tests/bloglist_api.test.js
@@ -18,13 +18,35 @@ test('there are two blogs', async () => {
 
     assert.strictEqual(response.body.length, 2)
 })
-test.only('field named id exists for every entry', async () => {
+
+test('field named id exists for every entry', async () => {
     const response = await api.get('/api/blogs')
 
     const body = response.body
     const allHaveIds = body.every(blog => blog.id !== undefined)
 
     assert.strictEqual(allHaveIds, true)
+})
+
+test('blogs can be added', async () => {
+    const initialState = await api.get('/api/blogs')
+
+    const body = {
+        author: "Test",
+        title: "Test blog",
+        url: "test.url",
+        votes: 0
+    }
+
+    await api
+        .post('/api/blogs')
+        .send(body)
+        .expect(201)
+        .expect('Content-Type', /application\/json/)
+
+    const response = await api.get('/api/blogs')
+
+    assert.strictEqual(response.body.length, initialState.body.length + 1)
 })
 
 after(async () => {
