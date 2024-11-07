446bc314b28954b561948648bdb1ffa504d4b802
mikaelti <mikaelti@metropolia.fi>
2024-11-07 03:05:44 +0200

    5.7

--- osa5/bloglist-frontend/src/components/Blog.jsx
+++ osa5/bloglist-frontend/src/components/Blog.jsx
@@ -1,7 +1,18 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from 'react'
+
+const Blog = ({ blog }) => {
+  const [show, setShow] = useState(false)
+  return(
+      <div style={{border: '1px solid'}}>
+        {blog.title} by {blog.author} {show?
+          <div>
+            <button onClick={() => setShow(false)}>hide</button>
+            <p style={{margin: '1px'}}>{blog.url}</p>
+            {blog.votes} votes <button>like</button>
+          </div>
+          :<button onClick={() => setShow(true)}>show</button>}
+      </div>
+  )
+}
 
 export default Blog
