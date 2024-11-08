29675732c78357b91699c9187740126d29d50488
mikaelti <mikaelti@metropolia.fi>
2024-11-08 13:51:03 +0200

    5.14

--- osa5/bloglist-frontend/src/components/Blog.jsx
+++ osa5/bloglist-frontend/src/components/Blog.jsx
@@ -3,9 +3,9 @@ import PropTypes from 'prop-types'
 
 const Blog = ({ blog, handleVote, loggedUser, handleDelete }) => {
     Blog.propTypes = {
-        blog: PropTypes.func.isRequired,
+        blog: PropTypes.object.isRequired,
         handleVote: PropTypes.func.isRequired,
-        loggedUser: PropTypes.func.isRequired,
+        loggedUser: PropTypes.object.isRequired,
         handleDelete: PropTypes.func.isRequired
     }
 
@@ -18,6 +18,7 @@ const Blog = ({ blog, handleVote, loggedUser, handleDelete }) => {
             <button onClick={() => setShow(false)}>hide</button>
             <p style={{margin: '1px'}}>{blog.url}</p>
             {blog.votes} votes <button onClick={() => handleVote(blog)}>like</button>
+              <p style={{margin: '1px'}}>{blog.user.name}</p>
               {(blog.user.id === loggedUser.id) && <button onClick={() => handleDelete(blog.id)}>remove</button> /*backend muokattu niin että antaa myös id:n */}
           </div>
           :<button onClick={() => setShow(true)}>show</button>}
--- osa5/bloglist-frontend/src/components/Blog.test.jsx
+++ osa5/bloglist-frontend/src/components/Blog.test.jsx
@@ -1,7 +1,8 @@
-import { render, screen } from '@testing-library/react'
+import {render, screen} from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import Blog from './Blog'
 
-test('renders content', () => {
+test('renders only author and title', () => {
     const blog = {
         author: 'Matti Meikalainen',
         title: 'Matin testi blogi',
@@ -17,4 +18,33 @@ test('renders content', () => {
     expect(urlText).toBeNull()
     const  votesText = screen.queryByText('0 votes')
     expect(votesText).toBeNull()
+})
+
+test('renders rest of content when show button is pressed', async () => {
+    const blog = {
+        author: 'Matti Meikalainen',
+        title: 'Matin testi blogi',
+        url: 'MattiTest.com',
+        votes: 0,
+        user: {
+            name: 'Matti Meikalainen',
+            id: 12345
+        }
+    }
+
+    const loggedUser = {
+        id: 12345
+    }
+
+    const { container } = render(<Blog blog={blog} loggedUser={loggedUser} />)
+
+    const user = userEvent.setup()
+    const showButton = screen.getByText('show')
+    await user.click(showButton)
+
+    const div = container.querySelector('.blog')
+    expect(div).toHaveTextContent('Matin testi blogi by Matti Meikalainen')
+    expect(div).toHaveTextContent('Matti Meikalainen')
+    expect(div).toHaveTextContent('MattiTest.com')
+    expect(div).toHaveTextContent('0 votes')
 })
