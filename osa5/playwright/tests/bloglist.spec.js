886c0bb730d1a1e8c4a1de0255c53bd198f5b6de
mikaelti <mikaelti@metropolia.fi>
2024-11-13 15:15:25 +0200

    5.19

--- osa5/playwright/tests/bloglist.spec.js
+++ osa5/playwright/tests/bloglist.spec.js
@@ -1,4 +1,5 @@
 const { test, expect, beforeEach, describe } = require('@playwright/test')
+const {text} = require("stream/consumers");
 
 describe('Bloglist app', () => {
     beforeEach(async ({ page, request }) => {
@@ -36,6 +37,25 @@ describe('Bloglist app', () => {
             await page.getByRole('textbox').last().fill('väärä')
             await page.getByRole('button', { name: 'login' }).click()
             await expect(page.getByText('wrong credentials')).toBeVisible()
+        })
+    })
+
+    describe('When logged in', () => {
+        beforeEach(async ({page}) => {
+            await page.getByRole('textbox').first().fill('mluukkai')
+            await page.getByRole('textbox').last().fill('salainen')
+            await page.getByRole('button', {name: 'login'}).click()
+        })
+
+        test('a new blog can be created', async ({page}) => {
+            await page.getByRole('button', {name: 'Add blog'}).click()
+            const textboxes = await page.getByRole('textbox').all()
+            await textboxes[0].fill("Matti Meikäläinen")
+            await textboxes[1].fill("Matin blogi")
+            await textboxes[2].fill("matti.com")
+            await page.getByRole('button', {name: 'Add blog'}).click()
+            await expect(page.getByText('A new blog Matin blogi by Matti Meikäläinen added')).toBeVisible()
+            await expect(page.locator('div.blog', { hasText: 'Matin blogi by Matti Meikäläinen'})).toBeVisible()
         })
     })
 })
