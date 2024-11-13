3f04cd7c6c5821be9a6e6d3c2dbffa354461ec96
mikaelti <mikaelti@metropolia.fi>
2024-11-13 14:14:43 +0200

    5.17

--- osa4/blogilista/controllers/testing.js
+++ osa4/blogilista/controllers/testing.js
@@ -3,6 +3,8 @@ const Blog = require('../models/blog')
 const User = require('../models/user')
 
 router.post('/reset', async (request, response) => {
+    console.log("RESETTING TEST DB")
+    console.log("---")
     await Blog.deleteMany({})
     await User.deleteMany({})
 
--- osa5/playwright/tests/bloglist.spec.js
+++ osa5/playwright/tests/bloglist.spec.js
@@ -2,7 +2,7 @@ const { test, expect, beforeEach, describe } = require('@playwright/test')
 
 describe('Bloglist app', () => {
     beforeEach(async ({ page, request }) => {
-        await request.post('http:localhost:3003/api/testing/reset')
+        await request.post('http://localhost:3003/api/testing/reset')
         await request.post('http://localhost:3003/api/users', {
             data: {
                 name: 'Matti Luukkainen',
@@ -15,7 +15,7 @@ describe('Bloglist app', () => {
     })
 
     test('Login form is shown', async ({ page }) => {
-        const locator = await page.getByText('Login')
+        const locator = await page.getByText('Login').first()
         await expect(locator).toBeVisible()
         await expect(page.getByText('username')).toBeVisible()
         await expect(page.getByText('password')).toBeVisible()
